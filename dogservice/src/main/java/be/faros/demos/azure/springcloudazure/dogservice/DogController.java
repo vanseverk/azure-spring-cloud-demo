a8ce071c98927214442a45a0e769fe6d61b56a19
Kristof Van Sever <vanseverk@gmail.com>
2020-02-01 11:47:39 +0100

    Demo applications

---
+++ dogservice/src/main/java/be/faros/demos/azure/springcloudazure/dogservice/DogController.java
@@ -0,0 +1,20 @@
+package be.faros.demos.azure.springcloudazure.dogservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DogController {
+
+    private final String dogMessage;
+
+    public DogController(@Value("${dogservice:message}") String dogMessage) {
+        this.dogMessage = dogMessage;
+    }
+
+    @GetMapping("/talk")
+    public String talk() {
+        return String.format("The dog says %s", dogMessage);
+    }
+}
---
+++ dogservice/src/main/java/be/faros/demos/azure/springcloudazure/dogservice/DogserviceApplication.java
@@ -0,0 +1,13 @@
+package be.faros.demos.azure.springcloudazure.dogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogserviceApplication.class, args);
+	}
+
+}
